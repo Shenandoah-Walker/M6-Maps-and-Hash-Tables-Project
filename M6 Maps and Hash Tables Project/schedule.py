fff03b92f042a3181297c0bd4633bb7633f78a20
Shenandoah_Walker <saw67402@email.vccs.edu>
2026-05-05 12:11:00 -0400

    Finished print_header and print methods

--- M6 Maps and Hash Tables Project/schedule.py
+++ M6 Maps and Hash Tables Project/schedule.py
@@ -11,3 +11,22 @@ class Schedule:
         self.dictionary[key] = item
 
     def print_header(self):
+        print(
+            f"{'Subject':<9}"
+            f"{'Catalog':<9}"
+            f"{'Section':<9}"
+            f"{'Component':<12}"
+            f"{'Session':<9}"
+            f"{'Units':>7}"
+            f"{'TotEnrl':>9}"
+            f"{'CapEnrl':>9}"
+            f"{'Instructor':<20}"
+        )
+
+    def print(self):
+        self.print_header()
+        for item in self.dictionary.values():
+            item.print()
+
+    def find_by_subject(self, subject):
+
