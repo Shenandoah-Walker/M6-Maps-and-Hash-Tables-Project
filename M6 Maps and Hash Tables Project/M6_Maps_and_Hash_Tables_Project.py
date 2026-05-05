ae898d8a175c2facd25defb3d8ff1910c23e8f55
Shenandoah_Walker <saw67402@email.vccs.edu>
2026-05-05 14:08:52 -0400

    Wrote most of test program

--- M6 Maps and Hash Tables Project/M6_Maps_and_Hash_Tables_Project.py
+++ M6 Maps and Hash Tables Project/M6_Maps_and_Hash_Tables_Project.py
@@ -1,1 +1,60 @@
+#Test program for M6 Maps and Hash Tables Project
+
+from schedule import Schedule
+
+schedule = Schedule("courses.csv")
+
+print("This program contains the VPCC STEM 2022 Summer Schedule of Classes.")
+print()
+print("Menu:")
+print("Please select from the options below. Enter the number of your choice.")
+print("1. Display all (print the full course schedule")
+print("2. Search by subject")
+print("3. Search by subject/catalog")
+print("4. Search by instructor last name")
+print("5. Quit")
+
+print()
+choice = input("Please enter your choice: ")
+
+if (choice == "1"):
+    schedule.print()
+
+elif (choice == "2"):
+    subject = input("Please enter the subject to search for (it should be in the following format: BIO): ")
+    results = schedule.find_by_subject(subject)
+    if results == None:
+        print("There are no courses with that subject.")
+
+    else:
+        schedule.print_header()
+        for item in results:
+            item.print()
+
+elif (choice == "3"):
+    subject = input("Please enter the subject to search for (it should be in the following format: BIO): ")
+    catalog = input("Please enter the catalog number to search for (it should be in the following format: 101): ")
+    results = schedule.find_by_subject_catalog(subject, catalog)
+    if results == None:
+        print("There are no courses with that subject and catalog number.")
+    else:
+        schedule.print_header()
+        for item in results:
+            item.print()
+
+elif (choice == "4"):
+    last_name = input("Please enter the instructor's last name to search for (it should be in the following format: Smith): ")
+    results = schedule.find_by_instructor_last_name(last_name)
+    if results == None:
+        print("There are no courses with that instructor.")
+    else:
+        schedule.print_header()
+        for item in results:
+            item.print()
+
+elif (choice == "5"):
+    print("End of program.")
+
+else:
+    print("Invalid choice. Choice must be a number between 1 and 5, inclusive.")
 
